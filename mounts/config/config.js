7235570c3132ac180d81ce79cc03c94f307c5d5a
Andrew Montgomery <andrewdmontgomery@mac.com>
2026-09-17 23:08:44 -0500

    chore: mount MMM-HourlyStrip in bottom_bar, drop invisible hourly

--- mounts/config/config.js
+++ mounts/config/config.js
@@ -49,15 +49,16 @@ let config = {
 			}
 		},
 		{
-			module: "weather",
+			module: "MMM-HourlyStrip",
+			position: "bottom_bar",
 			config: {
-				weatherProvider: "openmeteo",
-				type: "hourly",
 				lat: 44.8480,
 				lon: -93.0430,
 				units: "imperial",
-				maxNumberOfDays: 2,
-				updateInterval: 10 * 60 * 1000
+				hoursToShow: 24,
+				showPrecipThreshold: 20,
+				showSunrise: true,
+				showSunset: true
 			}
 		},
 		{
