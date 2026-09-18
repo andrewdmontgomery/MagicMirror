e4e3cc1c427255fcac908b573f78575572334fc6
Andrew Montgomery <andrewdmontgomery@mac.com>
2026-09-18 09:23:02 -0500

    fix: switch rain map to keyless Esri dark-gray basemap

--- mounts/config/config.js
+++ mounts/config/config.js
@@ -90,7 +90,7 @@ let config = {
 					{ lat: 44.8480, lng: -93.0430, color: "red" }
 				],
 				provider: "rainviewer",
-				mapUrl: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+				mapUrl: "https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}",
 				maxHistoryFrames: 6,
 				maxForecastFrames: 0,
 				updateIntervalInSeconds: 600
