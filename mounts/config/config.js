db11e9710adc9734c390a049a5d97e575dad9fa8
Andrew Montgomery <andrewdmontgomery@mac.com>
2026-09-18 09:52:05 -0500

    chore: unhide VectorRain for dev preview

--- mounts/config/config.js
+++ mounts/config/config.js
@@ -114,7 +114,6 @@ let config = {
 			module: "MMM-VectorRain",
 			position: "bottom_left",
 			header: "Rain Map (Vector)",
-			hiddenOnStartup: true,
 			config: {
 				lat: 44.8480,
 				lon: -93.0430
